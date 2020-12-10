318e88e47ca755d139935a92f02a4cc4f5a79c09
lisn0 <lisnoxvel@gmail.com>
2020-12-10 02:46:03 +0100

    date

--- js/main.js
+++ js/main.js
@@ -112,10 +112,10 @@
     // For demo preview end
     
 
-    // Use this for real timer date
-    /*  var timerdate = "2020/01/01"; */
-
-	$("#countdown").countdown(timerdate, function(event) {
+    Use this for real timer date
+    var timerdate = "2021/02/03";
+    
+    $("#countdown").countdown(timerdate, function(event) {
         $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
     });
 
