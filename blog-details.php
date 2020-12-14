5e97bb1bbc70d91ae5ed05b5bedb9ae51094b6fc
Kissy2 <illfight4uu@gmail.com>
2020-12-14 23:12:39 +0100

    db link

--- blog-details.php
+++ blog-details.php
@@ -6,7 +6,7 @@ $footer = '<!-- Footer Section Begin --> <footer class="footer-section"> <div cl
 $form = ' <div class="comment-section spad"> <div class="container"> <div class="row"> <div class="col-lg-12"> <div class="section-title"> <h3>Leave A Comment</h3> </div> </div> </div> <div class="row"> <div class="col-lg-8 m-auto"> <form action="#" class="comment-form"> <div class="row"> <div class="col-lg-4"> <input type="text" placeholder="Name"> </div> <div class="col-lg-4"> <input type="text" placeholder="Email"> </div> <div class="col-lg-4"> <input type="text" placeholder="Phone"> </div> <div class="col-lg-12 text-center"> <textarea placeholder="Messages"></textarea> <button type="submit" class="site-btn">Send Message</button> </div> </div> </form> </div> </div> </div> </div>';
 
 
-$conn = new PDO("mysql:host=localhost;dbname=blog", 'root');
+$conn = new PDO("mysql:host=localhost;dbname=fitstn_db", 'root','Sexmachine69');
 $stmt = $conn->prepare("SELECT * FROM blog where id = :x");
 $stmt->bindParam(':x', $_GET['q']);
 $stmt->execute();
