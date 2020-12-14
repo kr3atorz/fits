8b441a899a2e63d6edc7dbe02c5f1de4135788a6
lisn0 <lisnoxvel@gmail.com>
2020-12-14 22:02:38 +0100

    new index

--- js/main.js
+++ js/main.js
@@ -1,123 +1,80 @@
-/*  ---------------------------------------------------
-    Template Name: Manup
-    Description: Manup Event HTML Template
-    Author: Colorlib
-    Author URI: http://colorlib.com
-    Version: 1.0
-    Created: Colorlib
----------------------------------------------------------  */
+(function($) {
 
-'use strict';
+  $('#meal_preference').parent().append('<ul class="list-item" id="newmeal_preference" name="meal_preference"></ul>');
+  $('#meal_preference option').each(function(){
+      $('#newmeal_preference').append('<li value="' + $(this).val() + '">'+$(this).text()+'</li>');
+  });
+  $('#meal_preference').remove();
+  $('#newmeal_preference').attr('id', 'meal_preference');
+  $('#meal_preference li').first().addClass('init');
+  $("#meal_preference").on("click", ".init", function() {
+      $(this).closest("#meal_preference").children('li:not(.init)').toggle();
+  });
+  
+  var allOptions = $("#meal_preference").children('li:not(.init)');
+  $("#meal_preference").on("click", "li:not(.init)", function() {
+      allOptions.removeClass('selected');
+      $(this).addClass('selected');
+      $("#meal_preference").children('.init').html($(this).html());
+      allOptions.toggle();
+  });
 
-(function ($) {
-
-    /*------------------
-        Preloader
-    --------------------*/
-    $(window).on('load', function () {
-        $(".loader").fadeOut();
-        $("#preloder").delay(200).fadeOut("slow");
+  var marginSlider = document.getElementById('slider-margin');
+  if (marginSlider != undefined) {
+      noUiSlider.create(marginSlider, {
+            start: [500],
+            step: 10,
+            connect: [true, false],
+            tooltips: [true],
+            range: {
+                'min': 0,
+                'max': 1000
+            },
+            format: wNumb({
+                decimals: 0,
+                thousand: ',',
+                prefix: '$ ',
+            })
     });
+  }
+  $('#reset').on('click', function(){
+      $('#register-form').reset();
+  });
 
-    /*------------------
-        Background Set
-    --------------------*/
-    $('.set-bg').each(function () {
-        var bg = $(this).data('setbg');
-        $(this).css('background-image', 'url(' + bg + ')');
-    });
-
-    /*------------------
-		Navigation
-	--------------------*/
-    $(".mobile-menu").slicknav({
-        prependTo: '#mobile-menu-wrap',
-        allowParentLinks: true
-    });
-
-    /*------------------------
-		Partner Slider
-    ----------------------- */
-    $(".partner-logo").owlCarousel({
-        items: 6,
-        dots: false,
-        autoplay: true,
-        loop: true,
-        smartSpeed: 1200,
-        margin: 116,
-        responsive: {
-            320: {
-                items: 2,
-            },
-            480: {
-                items: 3,
-            },
-            768: {
-                items: 4,
-            },
-            992: {
-                items: 5,
-            },
-            1200: {
-                items: 6
-            }
+  $('#register-form').validate({
+    rules : {
+        first_name : {
+            required: true,
+        },
+        last_name : {
+            required: true,
+        },
+        company : {
+            required: true
+        },
+        email : {
+            required: true,
+            email : true
+        },
+        phone_number : {
+            required: true,
         }
+    },
+    onfocusout: function(element) {
+        $(element).valid();
+    },
+});
+
+    jQuery.extend(jQuery.validator.messages, {
+        required: "",
+        remote: "",
+        email: "",
+        url: "",
+        date: "",
+        dateISO: "",
+        number: "",
+        digits: "",
+        creditcard: "",
+        equalTo: ""
     });
-
-    /*------------------------
-		Testimonial Slider
-    ----------------------- */
-    $(".testimonial-slider").owlCarousel({
-        items: 2,
-        dots: false,
-        autoplay: false,
-        loop: true,
-        smartSpeed: 1200,
-        nav: true,
-        navText: ["<span class='fa fa-angle-left'></span>", "<span class='fa fa-angle-right'></span>"],
-        responsive: {
-            320: {
-                items: 1,
-            },
-            768: {
-                items: 2
-            }
-        }
-    });
-
-    /*------------------
-        Magnific Popup
-    --------------------*/
-    $('.video-popup').magnificPopup({
-        type: 'iframe'
-    });
-
-    /*------------------
-        CountDown
-    --------------------*/
-    // For demo preview
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-
-    if(mm == 12) {
-        mm = '01';
-        yyyy = yyyy + 1;
-    } else {
-        mm = parseInt(mm) + 1;
-        mm = String(mm).padStart(2, '0');
-    }
-    var timerdate = mm + '/' + dd + '/' + yyyy;
-    // For demo preview end
-    
-
-    // Use this for real timer date
-      var timerdate = "2021/02/03"; 
-
-	$("#countdown").countdown(timerdate, function(event) {
-
-        $(this).html(event.strftime("<div class='cd-item'><span>%D</span> <p>Days</p> </div>" + "<div class='cd-item'><span>%H</span> <p>Hrs</p> </div>" + "<div class='cd-item'><span>%M</span> <p>Mins</p> </div>" + "<div class='cd-item'><span>%S</span> <p>Secs</p> </div>"));
-    });
-
 })(jQuery);
